dac2069e7f29c9b418ff61116fcd239f10ba4ffe
cheburakshu <qa@saltstack.com>
2018-05-22 22:21:27 +0000

    Sanic web server added

--- pflow/app.py
+++ pflow/app.py
@@ -1,5 +1,8 @@
 from multiprocessing import Process, Queue, cpu_count, RLock
 import threading
+import uvloop
+import asyncio
+import sanic
 
 from .manager import Manager
 from .server import Server
@@ -9,6 +12,7 @@ class App(Server):
     def __init__(self, *args, **kwargs):
         super().__init__()
         self.queue = Queue()
+        self.SANIC_PORT = 8000
         Manager.register('get_queue', callable=lambda: self.queue)
         self.manager = Manager(address=('',50000), authkey=b'abracadabra')
         self.NUMBER_OF_PROCESSES = cpu_count() * 2
@@ -17,11 +21,13 @@ class App(Server):
     def starting(self):
         self.start_workers()
         self.manager_thread.start()
+        #self.sanic_thread.start()
 
     def start_workers(self):
         self.workers = []
         for _ in range(self.NUMBER_OF_PROCESSES):
             self.workers.append(Process(target=self.worker, args=(self.queue,)))
+        self.workers.append(Process(target=self.start_sanic_server, args=(self.SANIC_PORT,)))
 
         for w in self.workers:
             w.start()
@@ -29,6 +35,17 @@ class App(Server):
     def start_manager(self):
         server = self.manager.get_server()
         server.serve_forever()
+
+    def start_sanic_server(self, port):
+        app = sanic.Sanic()
+        kafka = Kafka()
+
+        @app.route('/sensor/<data>', methods=['POST',])
+        async def sensor_handler(request, data):
+            kafka.produce(request.body)
+            return sanic.response.json({'message':'Success'})
+
+        app.run(host='0.0.0.0', port=port)
 
     def worker(self, q):
         kafka = Kafka()
